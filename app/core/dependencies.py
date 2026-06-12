8bde63a75b9e83073dd2508ade2922eb37bf89ce
Mapessah Samwel <samwelodhiambomapesa@gmail.com>
2026-06-12 08:42:19 +0300

    Refactor user repository import and clean up code formatting in dependencies.py

--- app/core/dependencies.py
+++ app/core/dependencies.py
@@ -2,7 +2,7 @@ from typing import Generator
 from sqlalchemy.orm import Session
 from app.core.database import SessionLocal
 from fastapi import Depends, HTTPException, status
-from app.repositories.user_repository import get_user_by_id
+from app.repositories.user import get_user_by_id
 from app.core.config import get_settings
 from jose import JWTError, jwt
 from app.core.security import oauth2_scheme
@@ -11,6 +11,7 @@ settings = get_settings()
 SECRET_KEY = settings.SECRET_KEY
 ALGORITHM = settings.ALGORITHM
 
+
 def get_db() -> Generator[Session, None, None]:
     db = SessionLocal()
     try:
@@ -18,9 +19,9 @@ def get_db() -> Generator[Session, None, None]:
     finally:
         db.close()
 
+
 def get_current_user(
-    token: str = Depends(oauth2_scheme),
-    db: Session = Depends(get_db)
+    token: str = Depends(oauth2_scheme), db: Session = Depends(get_db)
 ):
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
@@ -28,22 +29,19 @@ def get_current_user(
 
         if user_id is None:
             raise HTTPException(
-                status_code=status.HTTP_401_UNAUTHORIZED,
-                detail="Invalid token"
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
             )
 
     except JWTError:
         raise HTTPException(
-            status_code=status.HTTP_401_UNAUTHORIZED,
-            detail="Invalid token"
+            status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid token"
         )
 
     user = get_user_by_id(db, user_id)
 
     if user is None:
-        raise HTTPException(
-            status_code=404,
-            detail="User not found"
-        )
+        raise HTTPException(status_code=404, detail="User not found")
 
     return user
+
+# requiire superuser
